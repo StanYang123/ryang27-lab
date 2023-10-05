fcceb90ad08ac48a76912c582a59220a57121ed4
itcast <hello@itcast.cn>
2023-10-05 15:06:06 +0800

    renew

--- src/main/java/edu/iit/sat/itmd4515/ryang27/ryang27lab1/Ryang27Lab1Application.java
+++ src/main/java/edu/iit/sat/itmd4515/ryang27/ryang27lab1/Ryang27Lab1Application.java
@@ -3,6 +3,9 @@ package edu.iit.sat.itmd4515.ryang27.ryang27lab1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class Ryang27Lab1Application {
 
